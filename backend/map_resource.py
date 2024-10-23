e1c9e78d98243549c15f21a3ffb9b53de1a5839b
thanhtulearncode <thanh_tu.vo@insa-cvl.fr>
2024-10-24 03:12:47 +0700

    Update map_resource.py

--- backend/map_resource.py
+++ backend/map_resource.py
@@ -1,26 +1,22 @@
 import pygame as pg
 
 class MapResource:
-    def __init__(self, resource_manager, resource_type, amount):
-        self.resource_manager = resource_manager
+    def __init__(self, resource_type, amount):
         self.resource_type = resource_type
         self.the_rest = amount
         self.resource_cooldown = pg.time.get_ticks()
         self.available = True
 
-    def mine(self, team=""):
+    def mine(self, player):
         if self.the_rest > 0 and self.available:
             now = pg.time.get_ticks()
             if now - self.resource_cooldown > 2000:  #2 secondes entre chaque collecte
                 self.the_rest -= 1
                 self.resource_cooldown = now
-
-                # Ajouter des ressources à l'equipe
-                if team == "Blue":
-                    self.resource_manager.starting_resources[self.resource_type] += 1
-                elif team == "Red":
-                    self.resource_manager.starting_resources_AI[self.resource_type] += 1
                 
+                # Ajouter des ressources à l'equipe
+                player.owned_resources[self.resource_type] += 1
+            
                 # Verifiez si les ressources sont epuisees
                 if self.the_rest <= 0:
                     self.available = False
@@ -30,16 +26,16 @@ class MapResource:
             return 0
 
 class Map_Tree(MapResource):
-    def __init__(self, resource_manager):
-        super().__init__(resource_manager, "Wood", 100)
+    def __init__(self):
+        super().__init__("Wood", 100)
 
 class Map_Gold(MapResource):
-    def __init__(self, resource_manager):
-        super().__init__(resource_manager, "Gold", 800)
+    def __init__(self):
+        super().__init__("Gold", 800)
 
 class Map_Farm(MapResource):
-    def __init__(self, resource_manager):
-        super().__init__(resource_manager, "Food", 300)
+    def __init__(self):
+        super().__init__("Food", 300)
 
-    def gather_food(self, team=""):
-        return self.mine(team) 
+    def gather_food(self,  player):
+        return self.mine(player) 
