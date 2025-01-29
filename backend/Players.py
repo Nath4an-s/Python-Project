da746769328e7413f09a0abe1fb857ca336ca1ff
Nathan Sacier <nathan.sacier@insa-cvl.fr>
2025-01-29 15:38:18 +0100

    CHeat code

--- backend/Players.py
+++ backend/Players.py
@@ -6,6 +6,9 @@ Marines_starting_resources = {"Wood": 20000, "Food": 20000, "Gold": 20000}
 # Player Class
 class Player:
     def __init__(self, name, civilization, ai_profile, player_id, color=None):
+        self.Means_starting_resources = {"Wood": 200, "Food": 50, "Gold": 50}
+        self.Leans_starting_resources = {"Wood": 2000, "Food": 2000, "Gold": 2000}
+        self.Marines_starting_resources = {"Wood": 20000, "Food": 20000, "Gold": 20000}
         self.name = name
         self.civilization = civilization
         self.units = []  # Initialize the units list
@@ -19,11 +22,11 @@ class Player:
         self.training_units = []
         self.id = player_id  # Add this line to store the player's ID
         if self.civilization == "Means":
-            self.owned_resources = Means_starting_resources
+            self.owned_resources = self.Means_starting_resources
         elif self.civilization == "Leans":
-            self.owned_resources = Leans_starting_resources
+            self.owned_resources = self.Leans_starting_resources
         elif self.civilization == "Marines":
-            self.owned_resources = Marines_starting_resources
+            self.owned_resources = self.Marines_starting_resources
         
     def __str__(self):
         return f"{self.name} ({self.civilization})"
